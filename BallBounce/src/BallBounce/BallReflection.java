a9f88bd3cbf8a62421e3dcabed9f2dbcbf061f1c
fes7713 <tun05036@temple.edu>
2021-04-25 11:01:04 +0900

    BallBounce

---
+++ BallBounce/src/BallBounce/BallReflection.java
@@ -0,0 +1,27 @@
+package BallBounce;
+
+import java.awt.Color;
+import java.lang.String;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class BallReflection extends JFrame {
+    final static int fps = 30;
+
+    public BallReflection() {
+        DrawPanel panel = new DrawPanel();
+        add(panel);
+
+        new Timer(fps, panel).start();
+    }
+
+    public static void main(String[] A00) {
+        JFrame frame = new BallReflection();
+        frame.setTitle("反射するボール");
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setBackground(Color.white);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
